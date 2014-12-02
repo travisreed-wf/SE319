293119e067cbeb1a245601fceb5aafa5a0137896
Travis Reed <travis.reed@webfilings.com>
2014-12-02 15:24:02 -0600

    updated models

--- Portfolio3/src/models.py
+++ Portfolio3/src/models.py
@@ -1,46 +1,16 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
-
 db = SQLAlchemy()
 
 
-class Course(db.Model):
-    __tablename__ = 'course'
+class Item(db.Model):
+    __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True)
     title = db.Column(db.String(255))
-    teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    tasks = db.relationship('Task', backref='course',
-                            lazy='dynamic')
+    thumbnail_path = db.Column(db.String(255))
+    file_path = db.Column(db.String(255))
+    description = db.Column(db.String(1024))
 
-    def __init__(self, name):
-        self.name = name
-        return
-
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name
-        }
-
-    def set_students(self, student_file):
-        try:
-            lines = student_file.read()
-            students = lines.split(",")
-            for email in students:
-                print email
-                user = User.query.filter_by(email=email).first()
-                if user:
-                    if self not in user.courses:
-                        user.courses.append(user)
-                    else:
-                        print "Student already enrolled in course: %s\n" % email
-                else:
-                    user = User(email, None, None)
-                    user.courses.append(self)
-                db.session.commit()
-        except:
-            raise
+    def __init__(self, title, file_path):
+        self.title = title
+        self.file_path = file_path
--- Portfolio3/src/settingslocal.py
+++ Portfolio3/src/settingslocal.py
@@ -2,4 +2,4 @@ DEBUG_MODE = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 RELOADER_BOOL = True
 SECRET_KEY = 'SuperSecretDevKey'
-SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/travisreed/Github/SE319/Portfolio2/database.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/travisreed/Github/SE319/Portfolio3/database.db'
