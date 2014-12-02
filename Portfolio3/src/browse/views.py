5c32eb6767ea0169e32e886cca79872237a5bf7f
Hartman2 <hartman2@iastate.edu>
2014-12-02 17:50:14 -0600

    Mergining front end changes and added browse stuff

---
+++ Portfolio3/src/browse/views.py
@@ -0,0 +1,22 @@
+import flask
+from flask import flash
+from flask import redirect
+from flask import render_template
+from flask import url_for
+from flask.views import MethodView
+import models
+import re
+import json
+
+
+class BrowseView(MethodView):
+
+    def get(self):
+        return render_template('browse.html')
+
+    def post(self):
+        name = flask.request.form.get('title')
+        desc = flask.request.form.get("description")
+        f = flask.request.files.get('file_path')
+        print name, desc, f
+        return "Successful"
