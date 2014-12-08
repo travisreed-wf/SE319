e4d0507991f589dd80334d835bd790e89302461f
Travis Reed <travis.reed@webfilings.com>
2014-12-08 17:29:19 -0600

    bufxies

--- Portfolio3/src/home/views.py
+++ Portfolio3/src/home/views.py
@@ -12,8 +12,8 @@ import json
 class HomeScreenView(MethodView):
 
     def get(self):
-		l = []
-		items = models.Item.query.all()
-		for item in items:
-			l.append((item, models.Thumbnail.query,filter_by(id=item.thumbnail_id).first()))
-		return render_template('home.html', items_and_thumbs=l)
+        l = []
+        items = models.Item.query.all()
+        for item in items:
+            l.append((item, models.Thumbnail.query.filter_by(id=item.thumbnail_id).first()))
+        return render_template('home.html', items_and_thumbs=l)
